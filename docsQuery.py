26e9fe039ad030d8988626dd3f4f68ce5c1040c6
Afridi Hasan <hasan.afridi04@gmail.com>
2024-04-17 15:56:50 -0700

    whoosh

--- docsQuery.py
+++ docsQuery.py
@@ -1,16 +1,19 @@
+import os
+import shutil
+from google.colab import files
 import sys
 import subprocess
-from google.colab import files
-import tempfile
-import shutil
-import os
-import time
+import docx
+import pptx
+from PyPDF2 import PdfReader 
+import textwrap
+from whoosh.index import create_in
+from whoosh.fields import Schema, TEXT
+from whoosh.qparser import QueryParser
+from whoosh import qparser
 
-
-
-library_names = ['langchain', 'langchain-openai', 'faiss-cpu', 'PyPDF2','python-docx', 'openai', 'tiktoken', 'python-pptx', 'textwrap', ]
-
-# Dynamically importing libraries
+# Dynamically importing libraries not directly related to Whoosh (handling these as you did originally)
+library_names = ['langchain', 'langchain-openai', 'PyPDF2', 'python-docx', 'openai', 'tiktoken', 'python-pptx', 'textwrap']
 for name in library_names:
     try:
         __import__(name)
@@ -18,104 +21,84 @@ for name in library_names:
         print(f"{name} not found. Installing {name}...")
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', name])
 
-
-from PyPDF2 import PdfReader 
-import textwrap
-import docx
-import pptx
-from langchain_openai import OpenAIEmbeddings, OpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_openai import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from getpass import getpass
 
+# Whoosh does not need a specific embeddings object
+chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
-
-
-#token adding
 if "OPENAI_API_KEY" in os.environ:
     print("Token already set.")
 else:
     token = getpass("Enter your OpenAI token: ")
     os.environ["OPENAI_API_KEY"] = str(token)
 
+def create_index(index_dir):
+    if not os.path.exists(index_dir):
+        os.makedirs(index_dir)
+    schema = Schema(content=TEXT(stored=True))
+    return create_in(index_dir, schema)
 
+def add_documents_to_index(indexer, texts):
+    writer = indexer.writer()
+    for text in texts:
+        writer.add_document(content=text)
+    writer.commit()
 
+def search_index(query, indexer):
+    with indexer.searcher() as searcher:
+        query_parser = QueryParser("content", indexer.schema)
+        query_obj = query_parser.parse(query)
+        results = searcher.search(query_obj)
+        return [hit['content'] for hit in results]
 
-
-# Downloading embeddings from OpenAI
-embeddings = OpenAIEmbeddings()
-chain = load_qa_chain(OpenAI(), chain_type="stuff")
-
-
-def extract_texts(root_files):
-    """
-    Text extractes from file and puts it in a list.
-    Supported file formats include: .pdf, .docx, .pptx
-    If multiple files are uploaded, contents will be merged together
-    Parameters:
-    - root_files: A list containing the paths of the files to be processed.
-    Returns:
-    - A FAISS index object that includes the embeddings of the extracted text segments.
-    """
+def extract_and_index_texts(root_files, index_dir):
     raw_text = ''
-
     for root_file in root_files:
         _, ext = os.path.splitext(root_file)
         if ext == '.pdf':
             with open(root_file, 'rb') as f:
                 reader = PdfReader(f)
-                for i in range(len(reader.pages)):
-                    page = reader.pages[i]
-                    raw_text += page.extract_text()
+                raw_text += ' '.join([page.extract_text() for page in reader.pages])
         elif ext == '.docx':
             doc = docx.Document(root_file)
-            for paragraph in doc.paragraphs:
-                raw_text += paragraph.text
+            raw_text += ' '.join([p.text for p in doc.paragraphs])
         elif ext == '.pptx':
             ppt = pptx.Presentation(root_file)
-            for slide in ppt.slides:
-                for shape in slide.shapes:
-                    if hasattr(shape, 'text'):
-                        raw_text += shape.text
-
-    # retreival we don't hit the token size limits. 
-    text_splitter = CharacterTextSplitter(        
-                                            separator = "\n",
-                                            chunk_size = 1000,
-                                            chunk_overlap  = 200,
-                                            length_function = len,
-                                        )
-
-    texts = text_splitter.split_text(raw_text)
-
-    docsearch = FAISS.from_texts(texts, embeddings)
-    return docsearch
-
-
-def run_query(query, docsearch):
-    """
-    Executes a search query on a PDF file utilizing the docsearch and chain libraries.
-    Parameters:
-    query: A string that specifies the query to be executed.
-    file: A PDFReader object that holds the PDF file to be queried.
-    Returns:
-    A string that includes the results from applying the chain library to the documents retrieved by the docsearch similarity search.
-    """
+            raw_text += ' '.join([shape.text for slide in ppt.slides for shape in slide.shapes if hasattr(shape, 'text')])
     
-    docs = docsearch.similarity_search(query)
-    return chain.run(input_documents=docs, question=query)
+    texts = textwrap.wrap(raw_text, 1000)
+    indexer = create_index(index_dir)
+    add_documents_to_index(indexer, texts)
+    return indexer
 
+def run_conversation(folder_path):
+    index_dir = os.path.join(folder_path, 'indexdir')
+    root_files = upload_file(folder_path)
+    indexer = extract_and_index_texts(root_files, index_dir)
+
+    count = 0
+    while True:
+        print("Question ", count + 1)
+        query = input(" Ask questions or type stop:\n ")
+        
+        if query.lower() == "stop":
+            print("Thanks.")
+            break
+        elif query == "":
+            print("Input is empty!")
+            continue
+        else:
+            docs = search_index(query, indexer)
+            response = chain.run(input_documents=docs, question=query)
+            wrapped_text = textwrap.wrap(response, width=100)
+            print("Answer:")
+            for line in wrapped_text:
+                print(line)
+            count += 1
 
 def upload_file(folder_path):
-    """
-    Uploads a file from the local file system and stores it in a specified directory.
-    Parameters:
-    folder_path: A string that indicates the directory where the file should be saved.
-    Returns:
-    A string that denotes the path of the file that has been uploaded.
-    """
-    
     uploaded = files.upload()
     root_file = []
 
@@ -126,39 +109,5 @@ def upload_file(folder_path):
         root_file.append(folder_path + "/" + filename)
         os.remove(filename)
 
-
     return root_file
 
-
-def run_conversation(folder_path):
-    """
-    Starts a dialogue with the user by continuously requesting input queries and processing them against a PDF file.
-    Parameters:
-    folder_path: A string that specifies the location of the folder containing the PDF file.
-    Returns:
-    Conducts a conversation based on the PDF file.
-    """
-    root_files = upload_file(folder_path)
-    # location of the pdf
-
-
-    docsearch = extract_texts(root_files)
-
-    count = 0
-    while True:
-        print("Question ", count + 1)
-
-        query = input(" Ask questions or type stop:\n ")
-        
-        if query.lower() == "stop":
-            print("Thanks.")
-            break
-        elif query == "":
-            print("Input is empty!")
-            continue
-        else:
-            wrapped_text = textwrap.wrap(run_query(query, docsearch), width=100)
-            print("Answer:")
-            for line in wrapped_text:
-                print(line)
-            count += 1
